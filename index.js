4fae6fa384eca379b185b47dd892fbb34bb261c6
Rich Hoyt <rhoyt10@gmail.com>
2022-05-21 10:41:14 -0400

    Refactor attack code

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ const gravity = 0.7
 
 class Sprite {
     // pass arguments as an object, so position and requirement does not matter
-    constructor({position, velocity, color = 'red'}) {
+    constructor({position, velocity, color = 'red', offset}) {
         this.position = position
         this.velocity = velocity
         this.width = 50
         this.height = 150
         this.lastKey
         this.attackBox = {
-            position: this.position,
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset: offset,
             width: 100,
             height: 50
         }
@@ -30,17 +34,21 @@ class Sprite {
         c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
         // Attack box
-        c.fillStyle = 'green'
-        c.fillRect(
-            this.attackBox.position.x, 
-            this.attackBox.position.y, 
-            this.attackBox.width, 
-            this.attackBox.height 
-        )
+        if (this.isAttacking) {
+            c.fillStyle = 'green'
+            c.fillRect(
+                this.attackBox.position.x, 
+                this.attackBox.position.y, 
+                this.attackBox.width, 
+                this.attackBox.height 
+            )
+        }
     }
 
     update() {
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -69,7 +77,11 @@ const player = new Sprite({
         x: 0,
         y: 0
     },
-    color: 'purple'
+    color: 'purple',
+    offset: {
+        x: 0,
+        y: 0
+    }
 })
 
 const enemy = new Sprite({
@@ -81,7 +93,11 @@ const enemy = new Sprite({
         x: 0,
         y: 0
     },
-    color: 'red'
+    color: 'red',
+    offset: {
+        x: -50,
+        y: 0
+    }
 })
 
 const keys = {
@@ -103,6 +119,15 @@ const keys = {
     ArrowUp: {
         pressed: false
     }
+}
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
 }
 
 function animate() {
@@ -129,13 +154,26 @@ function animate() {
     }
 
     // detect collision
-    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && 
-        player.attackBox.position.x <= enemy.position.x + enemy.width &&
-        player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-        player.attackBox.position.y <= enemy.position.y + enemy.height &&
-        player.isAttacking
+    if (
+        rectangularCollision( { 
+            rectangle1: player,
+            rectangle2: enemy
+        }) && 
+        player.isAttacking 
     ) {
-        console.log('hit!')
+        player.isAttacking = false
+        console.log('player lands hit!')
+    }
+
+    if (
+        rectangularCollision( { 
+            rectangle1: enemy,
+            rectangle2: player
+        }) && 
+        enemy.isAttacking 
+    ) {
+        enemy.isAttacking = false
+        console.log('enemy lands hit!')
     }
 }
 
@@ -169,6 +207,9 @@ window.addEventListener('keydown', (event) => {
             break
         case 'ArrowUp':
             enemy.velocity.y = -20
+            break
+        case 'ArrowDown':
+            enemy.attack()
             break        
     }
 })
