c1cefc0c36f396f7647c8b4656ebfda3d7ba5fd1
Rich Hoyt <rhoyt10@gmail.com>
2022-05-22 13:39:30 -0400

    Make healthbars functional

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -162,6 +163,8 @@ function animate() {
         player.isAttacking 
     ) {
         player.isAttacking = false
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
         console.log('player lands hit!')
     }
 
@@ -173,6 +176,8 @@ function animate() {
         enemy.isAttacking 
     ) {
         enemy.isAttacking = false
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
         console.log('enemy lands hit!')
     }
 }
